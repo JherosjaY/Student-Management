da9be0e89fea780296dec782dddc93e72a0bdf58
JherosjaY <151810228+JherosjaY@users.noreply.github.com>
2026-05-12 19:13:35 +0800

    Delete api directory

--- api/src/shared/middleware/error-handler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { errorHandler } from '../errors/app-error.js';
-
-export const handleErrors = () => {
-  return (error: Error, req: Request, res: Response, next: NextFunction) => {
-    errorHandler(error, req, res, next);
-  };
-};
